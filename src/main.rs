6c609be51d5a90a0c2d1bfe45373a89f924dbeab
Borna Cvitanić <borna.cvitanic@gmail.com>
2024-07-22 19:41:34 +0200

    Add copypasta and update main.rs to post the contents of files to the clipboard

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::Read;
 use std::path::{Path, PathBuf};
 use clap::{Parser};
 use walkdir::WalkDir;
+use copypasta::{ClipboardContext, ClipboardProvider};
 use crate::cli::{AppCommand, Cli, Options};
 
 mod cli;
@@ -35,31 +36,58 @@ fn aggregate(options: Options) {
         // Extension logic here
     }
 
-    walk_directory(&root_path);
+    let file_paths = get_file_paths(&root_path);
+
+    let contents = combine_file_contents(&root_path, &file_paths);
+    match contents {
+        None => { eprintln!("No content found.") }
+        Some(contents) => {
+            let mut ctx = ClipboardContext::new().unwrap();
+            ctx.set_contents(contents).unwrap();
+            println!("Copied contents to clipboard!")
+        }
+    }
 }
 
-fn walk_directory(root_path: &PathBuf) {
-    for entry in WalkDir::new(&root_path).into_iter().filter_map(|e| e.ok()) {
-        match make_relative(entry.path(), &root_path) {
+fn combine_file_contents(root_path: &PathBuf, file_paths: &Vec<PathBuf>) -> Option<String> {
+    let mut combined_result = String::new();
+    for file_path in file_paths.iter() {
+        match make_relative(file_path, &root_path) {
             Some(relative_path) => {
-                match File::open(entry.path()) {
+                let mut contents = String::new();
+                match File::open(file_path) {
                     Ok(mut file) => {
-                        let mut contents = String::new();
                         if let Err(err) = file.read_to_string(&mut contents) {
-                            eprintln!("Error reading file {}: {}", entry.path().display(), err);
+                            eprintln!("Error reading file {}: {}", file_path.display(), err);
                         } else {
-                            println!("//{}", relative_path.display());
-                            println!("{}\n", contents);
+                            combined_result.push_str(&format!("//{}\n", relative_path.display()));
+                            combined_result.push_str(&contents);
+                            combined_result.push('\n');
                         }
-                    },
+                    }
                     Err(err) => {
-                        eprintln!("Error opening file {}: {}", entry.path().display(), err);
-                    },
+                        eprintln!("Error opening file {}: {}", file_path.display(), err);
+                    }
                 }
-            },
+            }
             None => println!("The base path is not an ancestor of the absolute path."),
         }
     }
+    if combined_result.is_empty() {
+        None
+    } else {
+        Some(combined_result)
+    }
+}
+
+fn get_file_paths(root_path: &PathBuf) -> Vec<PathBuf> {
+    let mut files = Vec::new();
+    for entry in WalkDir::new(&root_path).into_iter().filter_map(|e| e.ok()) {
+        if entry.metadata().map(|m| m.is_file()).unwrap_or(false) {
+            files.push(entry.path().to_path_buf());
+        }
+    }
+    files
 }
 
 fn distribute(options: Options) {
@@ -75,7 +103,9 @@ fn distribute(options: Options) {
         // Extension logic here
     }
 
-    walk_directory(&root_path);
+    let mut ctx = ClipboardContext::new().unwrap();
+    let clipboard_text = ctx.get_contents().unwrap();
+    println!("{}", clipboard_text);
 }
 
 fn make_relative(absolute_path: &Path, base_path: &Path) -> Option<PathBuf> {
